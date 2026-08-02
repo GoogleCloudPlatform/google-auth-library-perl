0080110786088f2481d558d5ab2aad0de4f3f9fd
C.J. Collier <cjac@google.com>
2026-08-02 18:12:18 +0000

    fix(auth): fully fix CVE-2026-66901 and double BUILDARGS bypass
    - Extend URL validation to all credential types (UserRefreshCredentials, ImpersonatedServiceAccountCredentials).
    - Fix bypass in ExternalAccountCredentials via make_creds by correctly tracking pinned universes.
    - Update Changes file and bump version to 0.08.

--- Google-Auth/lib/Google/Auth.pm
+++ Google-Auth/lib/Google/Auth.pm
@@ -24,7 +24,7 @@ use Google::Auth::ComputeEngine;
 use Google::Auth::Exceptions;
 use XSLoader;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 XSLoader::load('Google::Auth', $VERSION);
 
 =head1 NAME
